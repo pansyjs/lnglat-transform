ed33d1bea74c98db2a780be49734470d0c45a8df
wangxingkang <wang_xingkang@qq.com>
2024-06-17 22:11:32 +0800

    chore: update commitlint config

--- commitlint.config.mjs
+++ commitlint.config.mjs
@@ -1,5 +1,8 @@
 const Configuration = {
   extends: ['@commitlint/config-conventional'],
-};
+  rules: {
+    'body-max-line-length': [2, 'always', 500],
+  },
+}
 
-export default Configuration;
+export default Configuration
